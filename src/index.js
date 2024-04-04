31ffd55e80690772baed943cf062b6935b29c814
Igor Kharchenko <i.s.kharchenko96@gmail.com>
2024-04-04 23:52:30 +0500

    ajs-method

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './css/style.css';
+
+import './js/Character';
+
+import './js/Bowman';
+
+import './js/Swordsman';
+
+import './js/Magician';
+
+import './js/Undead';
+
+import './js/Zombie';
+
+import './js/Daemon';
